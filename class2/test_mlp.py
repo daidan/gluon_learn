87e3a723441ad93039e71760c31f33a2111d31dd
daidan <daidan@192.168.1.104>
2017-11-07 14:48:00 +0800

    a

---
+++ class2/test_mlp.py
@@ -0,0 +1,90 @@
+# -*- coding: UTF-8 -*-
+
+'''
+Created on 2017年10月6日
+
+@author: daidan
+'''
+
+
+import sys
+sys.path.append('..')
+
+from mxnet import ndarray as nd
+from mxnet import autograd
+from mxnet import gluon
+
+def transform(data, label):
+    return data.astype('float32')/255, label.astype('float32')
+
+mnist_train = gluon.data.vision.FashionMNIST(train=True, transform=transform)
+mnist_test = gluon.data.vision.FashionMNIST(train=False, transform=transform)
+
+batch_size = 256
+train_data = gluon.data.DataLoader(mnist_train, batch_size, shuffle=True)
+test_data = gluon.data.DataLoader(mnist_test, batch_size, shuffle=False)
+
+num_inputs = 28*28
+num_outputs = 10
+
+num_hidden = 256
+weight_scale = .01
+
+W1 = nd.random_normal(shape=(num_inputs, num_hidden), scale=weight_scale)
+b1 = nd.zeros(num_hidden)
+
+W2 = nd.random_normal(shape=(num_hidden, num_outputs), scale=weight_scale)
+b2 = nd.zeros(num_outputs)
+
+params = [W1, b1, W2, b2]
+
+for param in params:
+    param.attach_grad()
+    
+def SGD(params, lr):
+    for param in params:
+        param[:] = param - lr * param.grad
+    
+def relu(X):
+    return nd.maximum(X, 0)
+
+def net(X):
+    X = X.reshape((-1, num_inputs))
+    h1 = relu(nd.dot(X, W1) + b1)
+    output = nd.dot(h1, W2) + b2
+    return output
+
+def accuracy(output, label):
+    return nd.mean(output.argmax(axis=1)==label).asscalar()
+
+def evaluate_accuracy(data_iterator, net):
+    acc = 0.
+    for data, label in data_iterator:
+        output = net(data)
+        acc += accuracy(output, label)
+    return acc / len(data_iterator)
+
+softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
+
+learning_rate = .5
+
+for epoch in range(5):
+    train_loss = 0.
+    train_acc = 0.
+    for data, label in train_data:
+        with autograd.record():
+            output = net(data)
+            loss = softmax_cross_entropy(output, label)
+        loss.backward()
+        SGD(params, learning_rate/batch_size)
+
+        train_loss += nd.mean(loss).asscalar()
+        train_acc += accuracy(output, label)
+
+    test_acc = evaluate_accuracy(test_data, net)
+    print("Epoch %d. Loss: %f, Train acc %f, Test acc %f" % (
+        epoch, train_loss/len(train_data),
+        train_acc/len(train_data), test_acc))
+
+if __name__ == '__main__':
+    pass
---
+++ class2/test_mlp_gluon.py
@@ -0,0 +1,8 @@
+'''
+Created on 2017年10月6日
+
+@author: daidan
+'''
+
+if __name__ == '__main__':
+    pass
