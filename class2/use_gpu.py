f6df1873a4124bc44d21a8dcf4e4cc38fd5396fd
superhy <superhy199148@hotmail.com>
2018-01-04 15:18:34 +0800

    2

--- class2/use_gpu.py
+++ class2/use_gpu.py
@@ -6,10 +6,17 @@ Created on 2017年12月28日
 @author: superhy
 '''
 
+import pip
+
 import mxnet as mx
 import mxnet.ndarray as nd
 
+if pip.main(['show', 'mxnet-cu80']) == 0:
+    print('use gpu')
+else:
+    print('use cpu')
+
 _CTX = mx.gpu()
 
-x = nd.array([1,2,3], ctx=_CTX)
+x = nd.array([1, 2, 3], ctx=_CTX)
 print(x)
